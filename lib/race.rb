44aff7db478d87f6da50a002a72600a119abfbfe
Nathan Trautenberg <Natester123@mac.com>
2023-09-15 11:56:41 -0600

    Stuck on logic for adding winner to race class.

--- lib/race.rb
+++ lib/race.rb
@@ -1,12 +1,37 @@
 class Race
-  attr_reader :office, :candidates
+  attr_reader :office, :candidates, :open
   def initialize(office)
     @office = office
     @candidates = []
+    @open = true
   end
 
   def register_candidate!(candidate)
     @candidates << candidate
   end
 
+  def open?
+    @open = true
+  end
+
+  def close!
+    @open = false
+  end
+
+  def winner
+    if @open == true
+      winner = false
+    else 
+      @candidates.map do |candidate|
+        @vote_count_array = candidate.votes
+      end
+      if 
+        @vote_count_array.nonzero? == nil
+        winner = "tie"
+      else
+        winner = @vote_count_array.max
+      end
+    end
+    return winner
+  end
 end
---
+++ spec/iteration_four_spec.rb
@@ -0,0 +1,33 @@
+require './lib/candidate'
+require './lib/race'
+require './lib/election'
+
+RSpec.describe "#Candidate set up" do
+  before(:each) do
+    @election = Election.new("2023")
+    @race_1 = Race.new("Texas Governor")
+    @candidate_1 = Candidate.new({name: "Diana D", party: :democrat})
+    @candidate_2 = Candidate.new({name: "Roberto R", party: :republican})
+  end
+
+  it 'is open' do
+    expect(@race_1.open?).to eq(true)
+  end
+
+  it 'is closed' do
+    @race_1.close!
+    expect(@race_1.open).to eq(false)
+  end
+
+  it 'has a winner' do
+    @race_1.register_candidate!(@candidate_1)
+    @race_1.register_candidate!(@candidate_2)
+    #require 'pry';binding.pry
+    expect(@race_1.winner).to eq(false)
+    @race_1.close!
+    expect(@race_1.winner).to eq("tie")
+    @candidate_1.vote_for!
+    expect(@race_1.winner).to eq(@candidate_1)
+  end
+
+end
